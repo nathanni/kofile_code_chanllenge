5aa140aa729a3e4ceebd8b437577a6a7bc28430d
Nathan Ni <nathanoct@gmail.com>
2016-11-18 15:41:33 -0500

    restapi

--- app/route/api.js
+++ app/route/api.js
@@ -3,29 +3,49 @@ const router = express.Router();
 
 
 var orderRouter = function (orders, fees, orders_with_fees, orders_with_distributions) {
+
+    //get all fees info
     router.get('/fees', (req, res) => {
         res.json(orders_with_fees);
     });
 
+
+    //get all distributions
     router.get('/distributions', (req, res) => {
         res.json(orders_with_distributions);
     });
 
+    //get fees by ids
     router.get('/fees/:id', (req, res) => {
         var id = req.params.id;
-        var foundData = orders_with_fees.filter((obj) => {
-            return obj['Order Id'] === id;
-        })[0]; // gets first match (probably only match)
-        res.send(foundData);
+        var idArr = [];
+        idArr = id.split(',');
+        var foundDatas = [];
+        for (var i in idArr) {
+            var foundData = orders_with_fees.filter((obj) => {
+                return obj['Order Id'] === idArr[i];
+            })[0]; // gets first match (probably only match)
+            foundDatas.push(foundData);
+        }
+        res.send(foundDatas);
     });
 
 
+    //get distibutions by ids
     router.get('/distributions/:id', (req, res) => {
         var id = req.params.id;
-        var foundData = orders_with_distributions.filter((obj) => {
-            return obj['Order Id'] === id;
-        })[0]; // gets first match (probably only match)
-        res.send(foundData);
+        var idArr = [];
+        idArr = id.split(',');
+        var foundDatas = [];
+        for (var i in idArr) {
+            var foundData = orders_with_distributions.filter((obj) => {
+                return obj['Order Id'] === idArr[i];
+            })[0]; // gets first match (probably only match)
+            foundDatas.push(foundData);
+        }
+
+        res.send(foundDatas);
+
     });
 
     return router;
